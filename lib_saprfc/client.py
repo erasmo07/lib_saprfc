cece2f56204abdb312924bce9723a4d949a3ba0c
Erasmo Montes <erasmomontes07@gmail.com>
2017-04-04 14:58:24 -0400

    Added: New method to inset values in sap and changed something in other methods.

--- lib_saprfc/client.py
+++ lib_saprfc/client.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from . import exceptions
 try:
     from sapnwrfc import base as sap_base
 except:
@@ -18,8 +19,7 @@ class ApiClient(object):
 
     def __init__(self):
         self._config()
-        self._connection = None
-        self._connect()
+        self._connection = self._connect()
 
     def _config(self):
         """
@@ -54,9 +54,13 @@ class ApiClient(object):
         Return:
             function: With all parameter was set
         """
-        for key, value in parameters.items():
-            if hasattr(function, key):
+        for key, value in parameters:
+            try:
                 getattr(function, key)(value)
+            except (AttributeError, TypeError):
+                message = "La function {0} no tiene el atributo {0}".format(
+                    function.name, key)
+                raise exceptions.DoesNotHaveAttribute(message)
         return function
 
     def _discover_function(self, rfc_function):
@@ -93,9 +97,38 @@ class ApiClient(object):
             # Execute Funtion in SAP
             function.invoke()
             return function.DATA.value
-        except Exception as error:
-            print error
+        except Exception as e:
+            print e
             sys.exit(1)
+
+    def post(self, rfc_function, value):
+        """
+        This method call a funtion in sap that insert values.
+
+        Parameters:
+            rfc_function str(): the name of function in sap.
+            values dict(): all values that resive the function.
+
+        Return:
+            Values send sap.
+        """
+        # Discover function in sap
+        function_discover = self._discover_function(rfc_function)
+
+        # Create call
+        function = function_discover.create_function_call()
+
+        # Set parameters
+        self._add_paramter(function, value)
+
+        # Execute Funtion in SAP
+        function.invoke()
+
+        if not hasattr(function, 'I_ERROR'):
+            if hasattr(function, 'DATA'):
+                return function.DATA.value
+            return
+        return function.I_ERROR.I_ERROR
 
     def __del__(self):
         print 'Cerrando conexion sap'
